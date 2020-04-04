d68bc1a96e7cca8fce68f4a5c9b1e970ecb60236
Nicholas Rode <nrode@MBPNR.hsd1.wa.comcast.net>
2020-04-03 17:54:56 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '940638193036317',
+        clientSecret: 'f2182090b288a0e38cd9689e5bcbc112'
+    }
 }
